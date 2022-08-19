b80058c6b7c195cdf6c7280266e63f953b8e42cb
ReinaSparklechu <Sparklechus@gmail.com>
2022-08-19 23:07:57 +0800

    added firebase capabilities

--- src/main/java/Xeia/Data/JdbcCustomerRepository.java
+++ src/main/java/Xeia/Data/JdbcCustomerRepository.java
@@ -1,6 +1,12 @@
 package Xeia.Data;
 
 import Xeia.Customer.Customer;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
@@ -9,6 +15,9 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -17,18 +26,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 
 @Repository
 public class JdbcCustomerRepository implements CustomerRepository {
     private JdbcTemplate jdbc;
+    private Firestore db;
     @Autowired
-    public JdbcCustomerRepository(JdbcTemplate jdbc) {
+    public JdbcCustomerRepository(JdbcTemplate jdbc) throws IOException, ExecutionException, InterruptedException {
         this.jdbc = jdbc;
+        FirebaseApp.initializeApp(options);
+        db = FirestoreClient.getFirestore();
+        List< QueryDocumentSnapshot> documents = db.collection("Reddit").get().get().getDocuments();
+
     }
+
+    // TODO: 19/8/2022 replace the repositories with firebase 
     @Override
     public Customer loginCustomer(String name, String passHash) {
         return null;
     }
+    FileInputStream serviceAccount = new FileInputStream("src/main/resources/reina-s-base-firebase-adminsdk-y5kk5-e559e59132.json");
+    FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
+
+
     private String convertToHex(final byte[] messageDigest) {
         BigInteger bigint = new BigInteger(1, messageDigest);
         String hexText = bigint.toString(16);
